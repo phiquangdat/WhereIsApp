d8259eeda4db3f95365e60cc26808396039ba4b0
Dat Phi <88042242+phiquangdat@users.noreply.github.com>
2025-05-10 18:26:07 +0300

    fix bugs

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,10 +1,13 @@
+/**
+ * @format
+ */
+
 import React from 'react';
-import {render} from '@testing-library/react-native';
-import App from '../App'; // Ensure this matches your project structure
-import {describe, expect, test} from '@jest/globals';
-describe('App', () => {
-  it('renders without crashing', () => {
-    const {getByText} = render(<App />);
-    expect(getByText('Where Is App')).toBeTruthy();
+import ReactTestRenderer from 'react-test-renderer';
+import App from '../App';
+
+test('renders correctly', async () => {
+  await ReactTestRenderer.act(() => {
+    ReactTestRenderer.create(<App />);
   });
 });
