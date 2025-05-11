c06c760d5b0a6fc403d0cc20d03f4831615f294e
phiquangdat <datphi020603@gmail.com>
2025-05-11 18:34:53 +0300

    deleted files

--- __mocks__/react-native-fs.js
+++
@@ -1,8 +0,0 @@
-export default {
-  readFile: jest.fn(() => Promise.resolve('mock file content')),
-  writeFile: jest.fn(() => Promise.resolve()),
-  unlink: jest.fn(() => Promise.resolve()),
-  exists: jest.fn(() => Promise.resolve(true)),
-  mkdir: jest.fn(() => Promise.resolve()),
-  // Add other methods as needed for your tests
-};
--- jest.setup.js
+++
@@ -1,3 +0,0 @@
-if (typeof global.setImmediate === 'undefined') {
-  global.setImmediate = (fn, ...args) => setTimeout(fn, 0, ...args);
-}
